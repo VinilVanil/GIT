de9c1d5a2d773fec96a5b41b597de2e5834eed40
VinilVanil <plus79777740970@gmail.com>
2022-05-12 20:45:35 +0800

    add def -> minus (добавил функциб минус)

--- far/nums.py
+++ far/nums.py
@@ -1,9 +1,16 @@
 def plus(a, b):
-    return a+b
+    return a + b
 
 
+def minus(a, b):
+    reyurn a - b
+
+
+text = "new text"
 if __name__ == '__main__':
     print(plus(2, 4))
+    print(minus(21, 22))
+
 
 
 
