cb454976cb5cf76f4ed502ca886ff5574ab5faea
VinilVanil <plus79777740970@gmail.com>
2022-05-13 15:50:15 +0800

    Add file string width def add_var (for add string)

--- far/nums.py
+++ far/nums.py
@@ -5,11 +5,19 @@ def plus(a, b):
 def minus(a, b):
     return a - b
 
+def delit(a, b):
+    if a<=b:
+        return a/b
+    if b>a:
+        return b/a
+
 
 text = "new text"
 if __name__ == '__main__':
     print(plus(2, 4))
     print(minus(21, 22))
+    print(delit(21, 22))
+
 
 
 
--- far/string.py
+++ far/string.py
@@ -1,10 +1,10 @@
 def add_var(a, b, c='', d=''):
-    return str(a) + str(b) + str(c) + str(d),
+    return str(a) + str(b) + str(c) + str(d)
 
 
 if __name__ == '__main__':
-    print ('fgdfr')
-print (add_var(2, 'Olo', 4))
+    print ('fgdfrgyu')
+print (add_var(2, 'Olo', 40))
 
 
 
