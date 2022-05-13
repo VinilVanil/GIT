6f24dc97c902f0368442c0d59c09f4f8924a6928
VinilVanil <plus79777740970@gmail.com>
2022-05-13 19:55:32 +0800

    Переделал в простой объект

--- far/nums.py
+++ far/nums.py
@@ -1,20 +1,23 @@
-def plus(a, b):
-    return a + b
+class calculations:
+    def __init__(self, per1, per2):
+        self.per1 = 0
+        self.per2 = 0
+
+    def plus(a, b):
+        return b
+
+    def minus(a, b) -> 'ВЫЧТЕТ ИЗ А Б':
+        return a - b
+
+    def delit(a, b):
+        if a <= b:
+            return b / a
+        if b > a:
+            return a / b
 
 
-def minus(a, b):
-    return a - b
-
-
-def delit(a, b):
-    if a <= b:
-        return a / b
-    if b > a:
-        return b / a
-
-
-text = "new text"
 if __name__ == '__main__':
-    print(plus(2, 4))
-    print(minus(21, 22))
-    print(delit(21, 22))
+    a = calculations
+    print(a.plus(2, 4))
+    print(a.minus(21, 22))
+    print(a.delit(4, 8))
